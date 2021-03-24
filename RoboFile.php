32edeee766183f26f6185d981583701457557e5f
Jonathan Martz <git@jmartz.de>
2021-03-24 06:45:06 +0100

    add deployment

---
+++ RoboFile.php
@@ -0,0 +1,61 @@
+<?php
+require('.robo/vendor/autoload.php');
+
+use \Robo\Tasks;
+
+use \JmartzGmbh\RoboConfig;
+
+class RoboFile extends Tasks{
+
+    use RoboConfig;
+
+    public function checkBuild(){
+        return 'success';
+    }
+
+    public function roboInstall(){
+        $this->_exec('cd .robo && composer install');
+    }
+
+    public function composerInstall(){
+        $this->_exec('cd src && composer install');
+    }
+
+    public function deploy(){
+        $config = $this->ConfigLoad('anton-config');
+
+        $user = $config['server']['user'];
+        $host = $config['server']['host'];
+        $domain = $config['server']['domain'];
+        $tmp = date('d-m-Y-H-i');
+
+        $this->taskRsync()
+            ->fromPath('./src/')
+            ->toHost($host)
+            ->toUser($user)
+            ->excludeVcs()
+            ->toPath('/var/www/' . $domain .'/releases/'.$tmp)
+            ->recursive()
+            ->run();
+
+        $this->taskSshExec($host, $user)
+        ->remoteDir('/var/www/' . $domain.'/releases/'.$tmp)
+        ->exec('yarn install')
+        ->run();
+
+        $this->taskSshExec($host, $user)
+        ->remoteDir('/var/www/' . $domain.'/releases')
+        ->exec('rm -rf current')
+        ->exec('mv '.$tmp.' current')
+        // ->exec('rm -rf '.$tmp)
+        ->run();
+
+        // fix permission
+        $this->taskSshExec($host, $user)
+        ->remoteDir('/var/www/' . $domain.'/releases/')
+        ->exec('chown -R www-data:www-data .')
+        ->run();
+
+        // delete old versions
+    }
+}
