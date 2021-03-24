d49f29d5ce583a780973f2f377d709af76bede6f
Jonathan Martz <git@jmartz.de>
2021-03-24 12:11:50 +0100

    update template header and footer

--- src/resources/views/components/footer.blade.php
+++ src/resources/views/components/footer.blade.php
@@ -1,7 +1,16 @@
 <div class="container">
     <div class="row">
-        <div class="col-12">
-            Footer
+        <div class="col-12 col-md-3">
+            Copyright by <a href="https://www.jmartz.de">Jonathan Martz</a>
+        </div>
+        <div class="col-12 col-md-3">
+            Impressum
+        </div>
+        <div class="col-12 col-md-3">
+
+        </div>
+        <div class="col-12 col-md-3">
+
         </div>
     </div>
 </div>
--- src/resources/views/components/header.blade.php
+++ src/resources/views/components/header.blade.php
@@ -1,7 +1,12 @@
 <div class="container">
     <div class="row">
-        <div class="col-12">
-            Header
+        <div class="col-12 col-md-6 mt-2">
+            <span class="logo">Oscillation</span>
+        </div>
+        <div class="col-12 col-md-6 mt-2">
+            <a href="/login" class="btn btn-secondary">Login</a>
+            <a href="/logout" class="btn btn-secondary">Logout</a>
+            <a href="/overview" class="btn btn-secondary">Overview</a>
         </div>
     </div>
 </div>
