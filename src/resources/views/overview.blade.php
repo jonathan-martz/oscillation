cbf7cd6ff21b67ce95194fecefd0fff631b99420
Jonathan Martz <git@jmartz.de>
2021-03-24 12:13:48 +0100

    start template overview

---
+++ src/resources/views/overview.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Oscillation - Overview</title>
+</head>
+
+<body class="antialiased">
+    <header>
+        <x-header />
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <?php
+
+                    ?>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <x-footer />
+    </footer>
+
+</body>
+<x-css />
+
+</html>
